13af4adffeec4d0727d456652e7219dcaac4ba99
thinonph <120686851+thinonph@users.noreply.github.com>
2026-08-04 23:06:29 -0700

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,25 @@
+local ui = loadstring(game:HttpGet("https://raw.githubusercontent.com/thinonph/ui/refs/heads/main/loader.lua"))()
+
+local launcher = ui.newLauncher({
+    title = 'WWW /waddlers',
+    version = 'v1'
+})
+
+launcher:addGame({
+    name = 'Bad Business',
+    status = 'up',
+    url = "https://raw.githubusercontent.com/DarkNetworks/Infinite-Yield/main/latest.lua"
+})
+
+launcher:addGame({
+    name = 'Arsenal',
+    status = 'updating',
+    url = "https://raw.githubusercontent.com/thinonph/ui/refs/heads/main/arsenal.lua"
+})
+
+launcher:bind('loaded', function(data)
+    if data.url then
+        loadstring(game:HttpGet(data.url))()
+    end
+    ui.notify(data.name .. ' ready')
+end)
